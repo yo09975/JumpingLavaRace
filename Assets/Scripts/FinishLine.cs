8d31a168d26ab671b67d9008898190b92e15e665
astoll <astoll620@gmail.com>
2013-08-03 23:10:46 -0400

    Added more course.

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishLine : MonoBehaviour {
+	
+	public GUIText back;
+	public GUIText front;
+	
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.tag == "Player")
+		{
+			Time.timeScale = 0;	
+			back.text = back.text.Replace("?", col.name);
+			front.text = front.text.Replace("?", col.name);
+			back.enabled = true;
+			front.enabled = true;
+		}
+	}
+}
